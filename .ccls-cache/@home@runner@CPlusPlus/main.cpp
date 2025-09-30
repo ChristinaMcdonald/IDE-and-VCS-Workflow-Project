fa3aebf4af75e96139a9f845d2fe2424872b1d94
Christina McDonald <48092565-cnm22816@users.noreply.replit.com>
2025-09-30 05:26:41 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@CPlusPlus/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char first, middle, last;
+  first = 'C';
+  middle = 'N';
+  last = 'M';
+  cout<<first<<middle<<last<<;
+  return 0;
+}
